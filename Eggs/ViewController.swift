b017b0a5c7b6628641e337c6cdc06413700efb21
Alisa <alisa.bajenova@gmail.com>
2024-09-20 17:03:50 +0300

    Initial Commit

---
+++ Eggs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eggs
+//
+//  Created by Алиса on 20.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
